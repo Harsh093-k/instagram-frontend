c029dc0a5e74c5de0a35327606869e82944f1158
Harsh093-k <harshkashyap701153@gmail.com>
2025-04-19 19:41:10 +0530

    done

--- src/components/followedUser.jsx
+++ src/components/followedUser.jsx
@@ -10,30 +10,28 @@ const ScrollablePersonList = ({ persons = [] }) => {
   };
 
   return (
-    <div className="  ml-10 ">
-      <div className="overflow-x-auto scrollbar-thin flex items-center justify-center mt-6">
-        <div className="flex gap-x-4 items-center">
-          {persons.map((person) => (
-            <div
-              key={person?._id}
-              className="flex flex-col items-center cursor-pointer min-w-[60px]"
-              onClick={() => sendPersonId(person?._id)}
-            >
-              {person?.profilePicture ? (
-                <img
-                  className="w-12 h-12 rounded-full border-2 border-white"
-                  src={person.profilePicture}
-                  alt={`${person?.username}'s Profile`}
-                />
-              ) : (
-                <FaUserCircle className="w-12 h-12 text-gray-400" />
-              )}
-              <p className="text-xs text-black text-center truncate w-14">
-                {person?.username}
-              </p>
-            </div>
-          ))}
-        </div>
+    <div className="w-full overflow-x-auto px-4 mt-6">
+      <div className="flex gap-x-4 w-max">
+        {persons.map((person) => (
+          <div
+            key={person?._id}
+            className="flex flex-col items-center cursor-pointer min-w-[60px]"
+            onClick={() => sendPersonId(person?._id)}
+          >
+            {person?.profilePicture ? (
+              <img
+                className="w-12 h-12 rounded-full border-2 border-white"
+                src={person.profilePicture}
+                alt={`${person?.username}'s Profile`}
+              />
+            ) : (
+              <FaUserCircle className="w-12 h-12 text-gray-400" />
+            )}
+            <p className="text-xs text-black text-center truncate w-14">
+              {person?.username}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
